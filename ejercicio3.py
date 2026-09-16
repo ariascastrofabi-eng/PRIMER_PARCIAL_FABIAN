b3157d011ac0b60981686ba0ce0f80168eea5993
Fabian Arias <ariascastrofabi@gmail.com>
2026-09-16 08:43:38 -0300

    EXAMEN

--- ejercicio3.py
+++ ejercicio3.py
@@ -3,7 +3,11 @@ numero_entero=int(input("INGRESA UN NUMERO ENTERO: "))
 if numero_entero < 11 or numero_entero < 0:
     for x in range(1,numero_entero+1):
         print(f"===========TABLA: {x}==========")
+        suma=0
         for i in range(1,13):
-            print(f"{x} x {i} = {x*i}")
+            resultado=x*i
+            print(f"{x} x {i} = {resultado}")
+            suma+=resultado
+        print(suma)
 else:
     print("debe ser de ese rango")
